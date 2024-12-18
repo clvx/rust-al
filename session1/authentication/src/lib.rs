4923b38d224186639bf7242bf9846b79f8fb4d9c
clvx <michael.ibarra@gmail.com>
2024-12-18 11:29:52 -0700

    structs and iterators

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -8,26 +8,59 @@ pub enum LoginAction {
     Denied,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole) -> User {
+        User {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role,
+        }
+    }
+}
+
+pub fn get_users() -> [User; 2] {
+    [
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("mike", "password", LoginRole::User),
+    ]
+}
+
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+
+    let users: [User; 2] = get_users(); 
     let username: String = username.to_lowercase();
 
-    if username != "admin" && username != "mike" {
-        return None;
+    /*
+    Searching for the User
+    users.iter():
+        Creates an iterator over the users array, where each item is a reference to a User (&User).
+    .find(|user: &&User| user.username == username):
+        The find method looks for the first User in the array where user.username == username.
+        |user: &&User|: A closure that takes a reference to a reference (&&User) because the iterator produces &User.
+    if let Some(user):
+        If a matching user is found, the Some(user) branch is executed.
+        The user variable is assigned the matching User.
+    */
+    if let Some(user) = users.iter().find(|user: &&User| user.username == username) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()));
+        } else {
+            return Some(LoginAction::Denied);
+        }
     }
-
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "mike" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
-    }
+    None
 }
 
 
