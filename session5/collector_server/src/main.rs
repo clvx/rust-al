be2aeea36a7ccf53cf97fd5f46f82aae2671a47b
clvx <michael.ibarra@gmail.com>
2025-01-31 11:01:36 -0700

    adding apis to fetch data

--- session5/collector_server/src/main.rs
+++ session5/collector_server/src/main.rs
@@ -1,4 +1,9 @@
 mod collector;
+use tokio::net::TcpListener;
+use axum::Extension;
+use axum::{Router, routing::get};
+
+mod api;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -9,10 +14,24 @@ async fn main() -> anyhow::Result<()> {
     // Get a database connection pool
     let pool = sqlx::SqlitePool::connect(&db_url).await?;
 
-    let handle = tokio::spawn(collector::data_collector(pool));
+    let handle = tokio::spawn(collector::data_collector(pool.clone()));
+
+
+
+    // Start the web server
+    let app = Router::new()
+        .route("/", get(test))
+        .route("/api/all", get(api::show_all))
+        .layer(Extension(pool)); // This is the database connection pool
+    let addr = TcpListener::bind("localhost:3000").await?;
+    axum::serve(addr, app).await?;
 
     // Wait for the data collector to finish
     handle.await??; // Two question marks - we're unwrapping the task result, and the result from running the collector.
     Ok(())
+}
+
+async fn test() -> &'static str {
+    "Hello, world!"
 }
 
