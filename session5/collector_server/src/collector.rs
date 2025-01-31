0ee9ac6f66e0973fbc93661875a269f3731ab77e
clvx <michael.ibarra@gmail.com>
2025-01-31 10:21:46 -0700

    adding db support for collector server

--- session5/collector_server/src/collector.rs
+++ session5/collector_server/src/collector.rs
@@ -1,23 +1,25 @@
+use sqlx::{Pool, Sqlite};
 use tokio::net::{TcpListener, TcpStream};
-use shared_data::{decode_v1, DATA_COLLECTOR_ADDRESS};
+use shared_data::{decode_v1, CollectorCommandV1, DATA_COLLECTOR_ADDRESS};
 use tokio::io::AsyncReadExt;
 use std::net::SocketAddr;
 
 
 
-pub async fn data_collector() -> anyhow::Result<()> {
+pub async fn data_collector(cnn: Pool<Sqlite>) -> anyhow::Result<()> {
     // Listen for TCP connections on the data collector address
     let listener = TcpListener::bind(DATA_COLLECTOR_ADDRESS).await?;
 
     // Loop forever, accepting connections
     loop {
         // Wait for a new connection
+        let cnn = cnn.clone();
         let (socket, address) = listener.accept().await?;
-        tokio::spawn(new_connection(socket, address));
+        tokio::spawn(new_connection(socket, address, cnn));
     }
 }
 
-async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
+async fn new_connection(mut socket: TcpStream, address: SocketAddr, cnn: Pool<Sqlite>) {
     println!("New connection from {address:?}");
     let mut buf = vec![0u8; 1024];
     loop {
@@ -33,6 +35,24 @@ async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
 
         println!("Received {n} bytes");
         let received_data = decode_v1(&buf[0..n]);
-        println!("Received data: {received_data:?}");
+        match received_data {
+            (timestampt, CollectorCommandV1::SubmitData { collector_id, total_memory, used_memory, average_cpu_usage }) => {
+                let collector_id = uuid::Uuid::from_u128(collector_id);
+                let collector_id = collector_id.to_string();
+
+                let result = sqlx::query("INSERT INTO timeseries (collector_id, received, total_memory, used_memory, average_cpu) VALUES (?, ?, ?, ?, ?)")
+                    .bind(&collector_id)
+                    .bind(timestampt)
+                    .bind(total_memory as i64)
+                    .bind(used_memory as i64)
+                    .bind(average_cpu_usage)
+                    .execute(&cnn)
+                    .await;
+
+                if result.is_err() {
+                    eprintln!("Failed to insert data: {result:?}");
+                }
+            }
+        }
     }
 }
