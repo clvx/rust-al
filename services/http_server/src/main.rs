f02557f672d2027b91e191022c3687e6ad5a8ae9
clvx <michael.ibarra@gmail.com>
2025-03-24 05:11:26 -0600

    2.4 simple state using tower

--- services/http_server/src/main.rs
+++ services/http_server/src/main.rs
@@ -3,19 +3,27 @@
 // routing is a module that contains the routing primitives like get, post, put, etc.
 // Router is a struct that is used to define the routes of the application.
 use axum::{
-    extract::{Path, Query},
-    response::Html,
-    routing::get,
-    Router,
+    extract::{Path, Query, State}, http::HeaderMap, response::Html, routing::get, Router
 };
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
+
+struct MyConfig {
+    config_string: String,
+}
 
 #[tokio::main]
 async fn main() {
+
+    let shared_config = Arc::new(MyConfig {
+        config_string: "My config string".to_string(),
+    });
+
     let app = Router::new()
         .route("/", get(handler))
         .route("/book/:id", get(path_extract))
-        .route("/book", get(query_extract));
+        .route("/book", get(query_extract))
+        .route("/headers", get(header_extract))
+        .with_state(shared_config);
 
     // It's called bind because it uses the bind() system call to bind to a socket.
     let listener = tokio::net::TcpListener::bind("localhost:3000")
@@ -26,8 +34,10 @@ async fn main() {
     axum::serve(listener, app).await.unwrap(); //axum is async
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn handler(
+    State(config): State<Arc<MyConfig>>
+    ) -> Html<String> {
+    Html(format!("<h1>{}</h1>", config.config_string))
 }
 
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
@@ -38,4 +48,10 @@ async fn query_extract(
     Query(params): Query<HashMap<String, String>>
 ) -> Html<String> {
     Html(format!("{params:#?}"))
+}
+
+async fn header_extract(
+    headers: HeaderMap
+    ) -> Html<String> {
+    Html(format!("{headers:#?}"))
 }
